129883e922ad70c86cccebc05e5ff9b4130dc2f6
Ivan Hahanov <ivanh@naliway.local>
2021-11-08 19:06:53 +0300

    add admin-panel

---
+++ admin-panel/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {TeamList} from "../components/team-list.component";
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <TeamList/>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
